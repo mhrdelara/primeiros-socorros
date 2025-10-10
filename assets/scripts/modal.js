ccc9c936dbcc7498a07147eb10352a0b399b265a
João Victor de Souza Vieira <joaovictorvieira012@gmail.com>
2025-10-10 20:21:35 +0000

    tela-inicial

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -1,9 +1,14 @@
 function closeModal(){
-  let modal = document.querySelector(".back-modal")
+  let bkModal = document.querySelector(".back-modal")
+  let modal = document.querySelector(".modal")
+  
+  bkModal.classList.add("disable")
   modal.classList.add("disable")
 }
 
 function showModal(){
-   let modal = document.querySelector(".back-modal")
+  let bkModal = document.querySelector(".back-modal")
+  let modal = document.querySelector(".modal")
   modal.classList.remove("disable")
+  bkModal.classList.remove("disable")
 }
