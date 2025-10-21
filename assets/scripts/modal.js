3c1515d953b1b4d3e5e1ecda60e305b5e118143d
João Victor de Souza Vieira <joaovictorvieira012@gmail.com>
2025-10-21 20:23:06 +0000

    tela-inicial

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -1,5 +1,6 @@
 let modalAberto = false;
 let modalDeleteAberto = false;
+let modalExitAberto = false;
 
 function showModal() {
   modalAberto = true;
@@ -18,7 +19,9 @@ function showModal() {
 }
 
 function closeModal() {
+  closeAllModals();
   if (modalDeleteAberto) return;
+  if (modalExitAberto) return;
 
   modalAberto = false;
   let bkModal = document.querySelector(".back-modal");
@@ -32,9 +35,13 @@ function showModalDelete() {
   modalDeleteAberto = true;
   let modal = document.querySelector(".confirm-modal-delete");
   let bkModal = document.querySelector(".back-modal");
+  let bkModalDelete = document.querySelector(".back-modal-delete");
 
   if (bkModal.classList.contains("disable")) {
     bkModal.classList.remove("disable");
+  }
+  if (bkModalDelete.classList.contains("disable")) {
+    bkModalDelete.classList.remove("disable");
   }
   if (modal.classList.contains("disable")) {
     modal.classList.remove("disable");
@@ -44,15 +51,21 @@ function showModalDelete() {
 function closeModalDelete() {
   modalDeleteAberto = false;
   let modal = document.querySelector(".confirm-modal-delete");
+  let bkModalDelete = document.querySelector(".back-modal-delete");
   modal.classList.add("disable");
+  bkModalDelete.classList.add("disable");
 }
 
 function showModalExit() {
   let modal = document.querySelector(".confirm-modal-exit");
   let bkModal = document.querySelector(".back-modal");
+  let bkModalExit = document.querySelector(".back-modal-exit");
 
   if (bkModal.classList.contains("disable")) {
     bkModal.classList.remove("disable");
+  }
+  if (bkModalExit.classList.contains("disable")) {
+    bkModalExit.classList.remove("disable");
   }
   if (modal.classList.contains("disable")) {
     modal.classList.remove("disable");
@@ -60,8 +73,11 @@ function showModalExit() {
 }
 
 function closeModalExit() {
+  modalExitAberto = false;
   let modal = document.querySelector(".confirm-modal-exit");
+  let bkModalExit = document.querySelector(".back-modal-exit");
   modal.classList.add("disable");
+  bkModalExit.classList.add("disable");
 }
 
 addEventListener("DOMContentLoaded", () => {
@@ -71,4 +87,51 @@ addEventListener("DOMContentLoaded", () => {
   document.querySelector("#btnNaoDelete").addEventListener("click", () => {
     closeModalDelete();
   });
+
+  document.querySelector("#btnSimExit").addEventListener("click", () => {
+    closeModalExit();
+  });
+
+  document.querySelector("#btnNaoExit").addEventListener("click", () => {
+    closeModalExit();
+  });
 });
+
+// back-model
+//   ---  modal
+//   ---  backmodal
+//   ------ excluir conta
+//   ------ sair conta
+//   --------  confirmação
+
+function closeAllModals() {
+  let backModalExit = document.querySelector(".back-modal-exit");
+  let confModalDelete = document.querySelector(".confirm-modal-delete");
+  let modal = document.querySelector(".modal");
+  let msgModalDelete = document.querySelector(".mensage-modal-delete");
+  let msgModalExit = document.querySelector(".confirm-modal-exit");
+  let backModal = document.querySelector(".back-modal");
+  let backModalDelete = document.querySelector(".back-modal-delete");
+
+  if (!backModalExit.classList.contains("disable")) {
+    backModalExit.classList.add("disable");
+  }
+  if (!confModalDelete.classList.contains("disable")) {
+    confModalDelete.classList.add("disable");
+  }
+  if (!modal.classList.contains("disable")) {
+    modal.classList.add("disable");
+  }
+  if (!msgModalDelete.classList.contains("disable")) {
+    msgModalDelete.classList.add("disable");
+  }
+  if (!msgModalExit.classList.contains("disable")) {
+    msgModalExit.classList.add("disable");
+  }
+  if (!backModal.classList.contains("disable")) {
+    backModal.classList.add("disable");
+  }
+  if (!backModalDelete.classList.contains("disable")) {
+    backModalDelete.classList.add("disable");
+  }
+}
