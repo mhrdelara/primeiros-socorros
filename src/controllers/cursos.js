b21cba0073ea63402cbe2c5f45928b3aea201e65
Gabi Martins <gabriellevitoriamartins2008@gmail.com>
2025-10-27 17:22:53 +0000

    banco de dados

---
+++ src/controllers/cursos.js
@@ -0,0 +1,58 @@
+const { Router } = require("express");
+const { db } = require("../db");
+const rotaCursos = Router();
+
+rotaCursos.get("/cursos", async (req, res) => {
+  const cursos = await db.cursos.findMany();
+  res.json(cursos);
+});
+
+rotaCursos.post("/cursos", async (req, res) => {
+  const { denuncia, texto, data_postagem, nota, like, dislike, id_usuarios } =
+    req.body;
+  await db.cursos.create({
+    data: {
+      denuncia,
+      texto,
+      data_postagem,
+      nota,
+      like,
+      dislike,
+      usuarios: {
+        connect: {
+          id: id_usuarios,
+        },
+      },
+    },
+  });
+  res.json({ mensagem: "OK" });
+});
+
+rotaCursos.delete("/cursos/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  await db.cursos.delete({
+    where: { id },
+  });
+  res.json({ mensagem: "OK" });
+});
+
+rotaCursos.put("/cursos/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  const data = {};
+
+  if (req.body.denuncia) data.denuncia = req.body.denuncia;
+  if (req.body.texto) data.texto = req.body.texto;
+  if (req.body.data_postagem) data.data_postagem = req.body.data_postagem;
+  if (req.body.nota) data.nota = req.body.nota;
+  if (req.body.like) data.like = req.body.like;
+  if (req.body.dislike) data.dislike = req.body.dislike;
+
+  await db.cursos.update({
+    where: { id },
+    data,
+  });
+
+  res.send({ mensagem: "OK" });
+});
+
+module.exports = { rotaCursos };
---
+++ src/controllers/materiais.js
@@ -0,0 +1,55 @@
+const { Router } = require("express");
+const { db } = require("../db");
+const rotaMateriais = Router();
+
+rotaMateriais.get("/materiais", async (req, res) => {
+  const materiais = await db.materiais.findMany();
+  res.json(materiais);
+});
+
+rotaMateriais.post("/materiais", async (req, res) => {
+  const { anexo, data_postagem, prova, like, dislike, id_usuarios } = req.body;
+  await db.materiais.create({
+    data: {
+      anexo,
+      texto,
+      data_postagem,
+      prova,
+      curso: {
+        connect: {
+          id: id_curso,
+        },
+      },
+    },
+  });
+  res.json({ mensagem: "OK" });
+});
+
+rotaMateriais.delete("/materiais/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  await db.materiais.delete({
+    where: { id },
+  });
+  res.json({ mensagem: "OK" });
+});
+
+rotaMateriais.put("/materiais/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  const data = {};
+
+  if (req.body.denuncia) data.denuncia = req.body.denuncia;
+  if (req.body.texto) data.texto = req.body.texto;
+  if (req.body.data_postagem) data.data_postagem = req.body.data_postagem;
+  if (req.body.nota) data.nota = req.body.nota;
+  if (req.body.like) data.like = req.body.like;
+  if (req.body.dislike) data.dislike = req.body.dislike;
+
+  await db.materiais.update({
+    where: { id },
+    data,
+  });
+
+  res.send({ mensagem: "OK" });
+});
+
+module.exports = { rotaMateriais };
