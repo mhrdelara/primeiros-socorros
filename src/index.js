3956f3ffe57405537d52369e30aa2aa4dfd5a137
Gabi Martins <gabriellevitoriamartins2008@gmail.com>
2025-10-31 17:43:40 +0000

    banco de dados atualizado

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const { db } = require("./db");
 const { rotaUsuario } = require("./controllers/usuario");
+const { rotaMateriais } = require("./controllers/materiais");
+const { rotaCursos } = require("./controllers/cursos");
 const server = express("");
 
 server.use(express.json());
 server.use(rotaUsuario);
+server.use(rotaMateriais);
+server.use(rotaCursos);
+
 server.listen(3000, console.log("Rodando"));
