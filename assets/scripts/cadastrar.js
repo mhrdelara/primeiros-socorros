d0c8cf5e5ffb98a1133105b39ef12160e99ec088
João Victor de Souza Vieira <joaovictorvieira012@gmail.com>
2025-11-12 20:23:31 +0000

    cadastrar-atualizado

--- assets/scripts/cadastrar.js
+++ assets/scripts/cadastrar.js
@@ -1,13 +1,27 @@
-const usuario = {
-  nome: nomeInput.value,
-  sobrenome: sobrenomeInput.value,
-  data_nascimento: data_nascimentoInput.value,
-  crm: crmInput.value,
-  funcao: nomeInput.value,
-  email: emailInput.value,
-  foto: localStorage.getItem("fotoPerfil") || "",
-};
+document.addEventListener("DOMContentLoaded", () => {
+  const nomeInput = document.querySelector("#nome");
+  const sobrenomeInput = document.querySelector("#sobrenome");
+  const data_nascimentoInput = document.querySelector("#data_nascimento");
+  const crmInput = document.querySelector("#crm");
+  const funcaoInput = document.querySelector("#funcao");
+  const emailInput = document.querySelector("#email");
+  const btnCadastrar = document.querySelector("#concluir");
 
-localStorage.setItem("usuarioLogado", JSON.stringify(usuario));
+  btnCadastrar.addEventListener("click", (e) => {
+    e.preventDefault();
 
-window.location.href = "../pages/index.html";
+    const usuario = {
+      nome: nomeInput.value.trim(),
+      sobrenome: sobrenomeInput.value.trim(),
+      data_nascimento: data_nascimentoInput.value,
+      crm: crmInput.value.trim(),
+      funcao: funcaoInput.value.trim(),
+      email: emailInput.value.trim(),
+      foto: localStorage.getItem("fotoPerfil") || "",
+    };
+
+    localStorage.setItem("usuarioLogado", JSON.stringify(usuario));
+
+    window.location.href = "../pages/tela-validacao.html";
+  });
+});
