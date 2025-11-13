59771be0aa0a78674dde2a6942c5948aa34f462e
Maria <mariaheloisarochadelara015@gmail.com>
2025-11-13 19:04:59 +0000

    alterações pages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { db } = require("./db");
+const path = require("path");
 const { rotaUsuario } = require("./controllers/usuario");
 const { rotaMateriais } = require("./controllers/materiais");
 const { rotaCursos } = require("./controllers/cursos");
@@ -10,8 +11,18 @@ server.use(rotaUsuario);
 server.use(rotaMateriais);
 server.use(rotaCursos);
 
+server.use(express.static(path.join(__dirname, "assets")));
+
 server.get("/", (req, res) => {
-  res.json(200);
+  res.sendFile(__dirname + "/pages/index.html");
+});
+
+server.get("/tela-cadastro", (req, res) => {
+  res.sendFile(__dirname + "../pages/tela-cadastro.html");
+});
+
+server.get("/tela-postagem", (req, res) => {
+  res.sendFile(__dirname + "../pages/tela-cadastro.html");
 });
 
 server.listen(3001, console.log("Rodando"));
