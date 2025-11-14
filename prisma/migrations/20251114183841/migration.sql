4cbf22c9ae046920a51ee34a87f8f4db86d93ad1
Maria <mariaheloisarochadelara015@gmail.com>
2025-11-14 18:39:30 +0000

    new

---
+++ prisma/migrations/20251114183841/migration.sql
@@ -0,0 +1,34 @@
+-- CreateTable
+CREATE TABLE "Usuario" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nome" TEXT NOT NULL,
+    "sobrenome" TEXT NOT NULL,
+    "data_nascimento" DATETIME NOT NULL,
+    "email" TEXT NOT NULL,
+    "crm" TEXT NOT NULL,
+    "funcao" TEXT NOT NULL,
+    "matricula" TEXT NOT NULL,
+    "foto_perfil" TEXT NOT NULL,
+    "senha" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Video" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "denuncia" INTEGER NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "data_postagem" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "like" TEXT NOT NULL,
+    "dislike" TEXT NOT NULL,
+    "titulo" TEXT NOT NULL,
+    "urlVideo" TEXT NOT NULL,
+    "usuarioId" INTEGER NOT NULL,
+    CONSTRAINT "Video_usuarioId_fkey" FOREIGN KEY ("usuarioId") REFERENCES "Usuario" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Material" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "anexo" TEXT NOT NULL,
+    "data_postagem" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
