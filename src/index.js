087640d0d4056e359c07d5271ca0d0cc96980651
Maria <mariaheloisarochadelara015@gmail.com>
2025-11-14 17:06:53 +0000

    index.js

--- src/index.js
+++ src/index.js
@@ -18,31 +18,31 @@ server.get("/", (req, res) => {
 });
 
 server.get("/tela-cadastro", (req, res) => {
-  res.sendFile(__dirname + "../pages/tela-cadastro.html");
+  res.sendFile(__dirname + "/pages/tela-cadastro.html");
 });
 
 server.get("/tela-postagem", (req, res) => {
-  res.sendFile(__dirname + "../pages/tela-postagem.html");
+  res.sendFile(__dirname + "/pages/tela-postagem.html");
 });
 
 server.get("/tela-alterar-usuario", (req, res) => {
-  res.sendFile(__dirname + "../pages/tela-alterar-usuario.html");
+  res.sendFile(__dirname + "/pages/tela-alterar-usuario.html");
 });
 
-server.get("/tela-validacao.html", (req, res) => {
-  res.sendFile(__dirname + "../pages/tela-validacao.html");
+server.get("/tela-validacao", (req, res) => {
+  res.sendFile(__dirname + "/pages/tela-validacao.html");
 });
 
-server.get("/tela-video.html", (req, res) => {
-  res.sendFile(__dirname + "../pages/tela-video.html");
+server.get("/tela-video", (req, res) => {
+  res.sendFile(__dirname + "/pages/tela-video.html");
 });
 
-server.get("/login.html", (req, res) => {
-  res.sendFile(__dirname + "../login.html");
+server.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/pages/login.html");
 });
 
-server.get("/recomendacao.html", (req, res) => {
-  res.sendFile(__dirname + "../recomendacao.html");
+server.get("/recomendacao", (req, res) => {
+  res.sendFile(__dirname + "/pages/recomendacao.html");
 });
 
 server.listen(3001, console.log("Rodando"));
